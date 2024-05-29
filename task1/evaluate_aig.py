aa4c5ae11cfef09c5f0eb588146b02aeab0aae3b
PangXH842 <pangxihern@gmail.com>
2024-05-29 10:39:20 +0800

    -

--- task1/evaluate_aig.py
+++ task1/evaluate_aig.py
@@ -6,6 +6,7 @@ circuitName, actions = state.split('_')
 circuitPath = './InitialAIG/train/' + circuitName + '.aig'
 libFile = './lib/7nm/7nm.lib'
 logFile = 'alu2.log'
+nextState = state + '.aig' # current AIG file
 
 # Evaluate AIG with yosys
 abcRunCmd = "./yosys-abc -c \""\
@@ -18,14 +19,6 @@ with open(logFile, 'r') as f:
     areaInformation = re.findall('[a-zA-Z0-9.]+', f.readlines()[-1])
     print(areaInformation)
     eval = float(areaInformation[-9]) * float(areaInformation[-4])
-
-
-state = 'alu2_0130622'
-circuitName, actions = state.split('_')
-circuitPath = './InitialAIG/train/' + circuitName + '.aig'
-libFile = './lib/7nm/7nm.lib'
-logFile = 'alu2.log'
-nextState = state + '.aig' # current AIG file
 
 # Regularize with resyn2
 RESYN2_CMD = "balance; rewrite; refactor; balance; rewrite; "\
@@ -43,3 +36,5 @@ with open(logFile, 'r') as f:
     areaInformation = re.findall('[a-zA-Z0-9.]+', f.readlines()[-1])
     baseline = float(areaInformation[-9]) * float(areaInformation[-4])
 eval = 1 - eval/baseline
+
+print(eval)
