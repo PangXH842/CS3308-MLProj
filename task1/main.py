adbb60af9694f150236daca36b7191ad7dd04631
PangXH842 <pangxihern@gmail.com>
2024-06-12 18:23:31 +0800

    -

--- task1/main.py
+++ task1/main.py
@@ -12,6 +12,7 @@ from generate_data import generate_data
 from utils import log, init_log, generate_line_graph
 
 def load_data(folder_path, samples_per_folder, log_file):
+    states_list = []
     graphs = []
     for circuit_type in os.listdir(folder_path):
         circuit_type_path = os.path.join(folder_path, circuit_type)
@@ -25,6 +26,9 @@ def load_data(folder_path, samples_per_folder, log_file):
             with open(states_file_path, 'rb') as f:
                 states = pickle.load(f)
                 for state, score in zip(states['input'], states['target']):
+                    if state in states_list:
+                        continue
+                    states_list.append(state)
                     graph = generate_data(state, score)
                     graphs.append(graph)
     return graphs
@@ -48,7 +52,7 @@ def main(args):
     test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
 
     # Build GCN model
-    model = GCN()
+    model = GCN(args.hidden_dims)
 
     # Loss function: Mean Square Error
     criterion = torch.nn.MSELoss()
@@ -111,6 +115,7 @@ if __name__ == "__main__":
     parser.add_argument('--learning_rate', type=float, default=0.01)
     parser.add_argument('--samples_per_folder', type=int, default=5)
     parser.add_argument('--batch_size', type=int, default=10)
+    parser.add_argument('--hidden_dims', type=int, default=16)
     parser.add_argument('--save_model_path', type=str, default="model_weights.pth")
     parser.add_argument('--save_plot_path', type=str, default="loss_graph.png")
     args = parser.parse_args()
